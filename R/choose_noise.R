6b11ae57dd7ef0403d844db1727169bee25a1635
Jack Kennedy <jack_kennedy1@hotmail.co.uk>
2022-10-11 20:17:24 +0100

    lint

--- R/choose_noise.R
+++ R/choose_noise.R
@@ -2,11 +2,11 @@
 #'
 #' Used when the user does not specify a creepy noise
 #'
-choose_noise = function() {
-  noises = c("creature", "eerie", "intense", "laugh",
+choose_noise <- function() {
+  noises <- c("creature", "eerie", "intense", "laugh",
              "piano", "piano2", "ringing", "strings")
-  chosen_noise = sample(noises,  1)
-  sound_path = system.file(
+  chosen_noise <- sample(noises,  1)
+  sound_path <- system.file(
     paste0("extdata/sounds/", chosen_noise, ".wav"),
     package = "creepr")
 }
--- R/creepr.R
+++ R/creepr.R
@@ -3,15 +3,16 @@
 #' This function plays a creepy noise
 #' @import beepr
 #' @param noise The chosen noise to play. Must be one of
-#'  <insert list of noises>
+#'  "creature", "eerie", "intense", "laugh"
+#'  "piano", "piano2", "ringing" or "strings"
 #'  If no noise is chosen, a random noise is played.
 #' @export
-creepr = function(noise) {
+creepr <- function(noise) {
   if(missing(noise)) {
-    noise = choose_noise()
+    noise <- choose_noise()
     beepr:::play_file(noise)
   } else {
-    sound_path = system.file(
+    sound_path <- system.file(
       paste0("extdata/sounds/", noise, ".wav"),
       package = "creepr")
 
