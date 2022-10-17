ef5f7dc51655d15d0fcc8a55c4539dfffd9d7ea9
Jack Kennedy <jack_kennedy1@hotmail.co.uk>
2022-10-17 13:33:56 +0100

    Add @param + description to choose_noise

--- R/choose_noise.R
+++ R/choose_noise.R
@@ -2,6 +2,7 @@
 #'
 #' Used when the user does not specify a creepy noise
 #'
+#' @param noises Vector of noise names
 choose_noise <- function(noises) {
   chosen_noise <- sample(noises,  1)
   system.file(
